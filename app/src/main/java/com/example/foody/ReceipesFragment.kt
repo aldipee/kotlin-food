d38e553b49598259eb6aeff46dc5dd5449c369b0
Aldi Pranata <aldipeee@gmail.com>
2021-02-15 15:22:48 +0700

    Add shimmer loading

--- app/src/main/java/com/example/foody/ReceipesFragment.kt
+++ app/src/main/java/com/example/foody/ReceipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_receipes.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +24,11 @@ class ReceipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_receipes, container, false)
+
+//        view.shimmer_recycler_view.showShimmer()
+
+        return view
     }
 
 }
